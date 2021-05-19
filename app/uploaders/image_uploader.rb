48bc2e416078e0171fb600ee944cfdef16a3a975
koki kanasaka <k.koki980701@gmail.com>
2021-05-19 22:06:38 +0900

    画像サイズ変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_fill: [300, 400]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
