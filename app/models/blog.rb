eb082295d2181e16fe5c3bad0365e8fb8e1219d2
koki kanasaka <k.koki980701@gmail.com>
2021-05-19 21:40:45 +0900

    写真を表示する機能

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
+  mount_uploader :image, ImageUploader
   validates :content, {presence: true, length: {maximum: 140}}
   belongs_to :user
-  mount_uploader :image, ImageUploader
 end
