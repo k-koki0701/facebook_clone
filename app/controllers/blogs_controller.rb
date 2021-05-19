fddcc16c0be0c42ed5cdb4fd1f2020f4e2da57d8
koki kanasaka <k.koki980701@gmail.com>
2021-05-19 13:49:22 +0900

    共通処理のメソッド化

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
   def index
     @blogs = Blog.all
   end
@@ -14,10 +15,21 @@ class BlogsController < ApplicationController
     end
   end
   def show
-    @blog = Blog.find(params[:id])
+  end
+  def edit
+  end
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
   private
   def blog_params
     params.require(:blog).permit(:content)
+  end
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
